d8043b296a29364ecdf83c407c45b4bf4124d9fd
fresnel <ngomafresnel@gmail.com>
2021-03-20 00:03:38 +0100

    Integration de firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
+import { config } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projet1';
+  constructor(){
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const config = {
+      apiKey: "AIzaSyCGvqwmcW0t14XtkQn4syrHEJIR8jtiON4",
+      authDomain: "gestion-livre-6ce39.firebaseapp.com",
+      projectId: "gestion-livre-6ce39",
+      storageBucket: "gestion-livre-6ce39.appspot.com",
+      messagingSenderId: "398310891232",
+      appId: "1:398310891232:web:3082832adcc402a65dfa85",
+      measurementId: "G-8S92GV9Y4C"
+    };
+    firebase.initializeApp(config);
+  }
 }
